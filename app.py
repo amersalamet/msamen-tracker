d631bdb8657be59b9310084070eb987ce74448e9
amersalamet <51185590+amersalamet@users.noreply.github.com>
2025-06-16 13:47:43 +0100

    Update app.py
    Binding

--- app.py
+++ app.py
@@ -77,5 +77,9 @@ def delete_expenses():
     conn.close()
     return redirect('/')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's port or default to 5000
+    app.run(host='0.0.0.0', port=port)
+
